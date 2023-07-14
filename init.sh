bafd8c72d0a1dadd524fbc178a217f4c55b72a65
Yibotian <yiboxiaotian@nuaa.edu.cn>
2023-07-15 07:06:00 +0800

    dotfiles_2023-07-15 from yibotian@Nicolette86132

--- init.sh
+++ init.sh
@@ -9,10 +9,10 @@ GITHUB_REPO="https://github.com/Ebotian/.dotfiles.git"
 # Change to the dotfiles repository directory
 cd "$DOTFILES_REPO"
 # Create symbolic links for all dotfiles in the repository
-find "$DOTFILES_REPO" -maxdepth 1 -type f -iregex '^.*$' -exec ln -sf {} $HOME \;
+find "$DOTFILES_REPO" -maxdepth 1 -type d,f -iregex '^.*/\..+$' ! -name '.git' -exec ln -sf {} $HOME \;
 
 # Move all dotfiles from $HOME to the repository
-find $HOME -maxdepth 1 -type f -iregex '^.*$' | xargs -I {} mv {} "$DOTFILES_REPO"
+find $HOME -maxdepth 1 -type f -iregex '^.*/\..+$' | xargs -I {} mv {} "$DOTFILES_REPO"
 # {} is used for arguments replace, xargs pass stdin to `{}` referred by its argument `-I` and replace `{}` in the later `mv`'s argument
 # {} is replaced by the current file name). The \; at the end of the command is used to terminate the -exec expression
 
